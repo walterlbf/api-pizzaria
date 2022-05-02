9160245572ff93f29ae995e4d8daa8527c3d0487
walterlbf <walter.lbf.f@gmail.com>
2022-05-01 21:38:43 -0400

    commit inicial

---
+++ src/main/java/com/pizzaria/api/PizzariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.pizzaria.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PizzariaApiApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/pizzaria/api/controller/Pizzas.java
@@ -0,0 +1,11 @@
+package com.pizzaria.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pizzas")
+public class Pizzas {
+
+
+}
