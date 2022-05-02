cd6c79e81de8b3c5d135a6627a21868932aa7d62
walterlbf <walter.lbf.f@gmail.com>
2022-05-01 23:57:00 -0400

    metodo que puxa todas as pizzas

--- src/main/java/com/pizzaria/api/controller/Pizzas.java
+++ src/main/java/com/pizzaria/api/controller/Pizzas.java
@@ -1,18 +1,23 @@
 package com.pizzaria.api.controller;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.pizzaria.api.model.Pizza;
+import com.pizzaria.api.repository.PizzaRepository;
 
 @RestController // é um controller
 @RequestMapping("/pizzas") // mapaeira as requisicoes
 public class Pizzas {
 
-  @GetMapping // quando chegar uma requisição get pizzas.
-  public List<Pizza> lista() {
+  @Autowired
+  private PizzaRepository pizzaRepository;
 
+  @GetMapping // quando chegar uma requisição get pizzas.
+  public List<Pizza> findAll() {
+    return pizzaRepository.findAll();
   }
 
 }
