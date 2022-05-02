89e856964c7c0b17ad081b15fc6eb3fa11285909
walterlbf <walter.lbf.f@gmail.com>
2022-05-01 23:40:23 -0400

    interface herdando metodos

--- src/main/java/com/pizzaria/api/repository/PizzaRepository.java
+++ src/main/java/com/pizzaria/api/repository/PizzaRepository.java
@@ -1,8 +1,10 @@
 package com.pizzaria.api.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.pizzaria.api.model.Pizza;
 
 @Repository
-public interface PizzaRepository {
+public interface PizzaRepository extends JpaRepository<Pizza, Long> {
 
 }
