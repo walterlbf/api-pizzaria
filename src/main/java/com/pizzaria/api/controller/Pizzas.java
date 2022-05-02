b318cbd4ffb270b33d8f371f2cbe16c91b5c012b
walterlbf <walter.lbf.f@gmail.com>
2022-05-01 22:05:24 -0400

    requicao funcionando no endpoint /pizzas

--- src/main/java/com/pizzaria/api/controller/Pizzas.java
+++ src/main/java/com/pizzaria/api/controller/Pizzas.java
@@ -1,11 +1,17 @@
 package com.pizzaria.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/pizzas")
+@RestController // é um controller
+@RequestMapping("/pizzas") // mapaeira as requisicoes
 public class Pizzas {
 
+  @GetMapping // quando chegar uma requisição get pizzas.
+  public String list() {
+    return "olá mundo!";
+
+  }
 
 }
